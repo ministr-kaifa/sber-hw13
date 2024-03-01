90189e7b56e27c08a7813c106ba401c479b93aa0
ministr-kaifa <eyzeep@yandex.ru>
2024-03-01 18:13:31 +0300

    hw: review ready

---
+++ back/src/test/java/ru/zubkoff/sber/hw13/Hw13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zubkoff.sber.hw13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
